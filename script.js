d5a055c7ee3cd2767f426f5feebc3cc14870a415
Artur Arsalanov <joomrise@gmail.com>
2017-12-30 02:17:55 +0300

    add smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+  var waypoint = new Waypoint({
+    element: document.getElementById('waypoint'),
+    handler: function() {
+      $('#row-with-navbar').toggleClass('sticky');
+    }
+  });
 
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if(/^#/.test(this.href) !== true) {
+      var target = $(this.hash);
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
 });
