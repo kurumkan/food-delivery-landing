835a153bc75fa9d86352632eab810ed005560499
Artur Arsalanov <joomrise@gmail.com>
2017-12-30 03:20:57 +0300

    add animation on scroll

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ $(document).ready(function() {
       }, 1000);
     }
   });
+
+  $('.js-animation-1').waypoint(function() {
+    $('.js-animation-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js-animation-2').waypoint(function() {
+    $('.js-animation-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js-animation-3').waypoint(function() {
+    $('.js-animation-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js-animation-4').waypoint(function() {
+    $('.js-animation-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
 });
